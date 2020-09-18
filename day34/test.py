b5726e63fb6caae6cb82505be22948a4bc3f9aeb
liyuxing123123 <lyx_liyuxing@163.com>
2020-09-18 22:41:35 +0800

    test123

---
+++ day34/test.py
@@ -0,0 +1,13 @@
+#/python3
+
+
+import pygame
+def update_screen(ai_settings, screen, ship):
+    """更新屏幕上的图像，并切换到新屏幕"""
+    # 每次循环时都重绘屏幕
+    screen.fill(ai_settings.bg_color)
+    ship.blitme()
+
+    # 让最近绘制的屏幕可见
+    pygame.display.flip()
+
