0067081c3a59d6029896dcc84ab2c7f1be6c62e3
liyuxing123123 <lyx_liyuxing@163.com>
2020-09-20 14:17:29 +0800

    test1212321233123

--- Download_Data/world_population1.py
+++ Download_Data/world_population1.py
@@ -2,7 +2,12 @@
 
 #/python3
 
+
+
 import json
+import pygal
+from pygal.style import RotateStyle
+from pygal.style import LightColorizedStyle
 import pygal_maps_world.maps
 from Download_Data.country_codes import get_country_code
 
@@ -27,10 +32,28 @@ for pop_dict in pop_data:
         # else:
         #     print('ERROR - ' + country_name)
         # print(country_name + ": " + str(population))
-wm = pygal_maps_world.maps.World()
-wm.title = 'World Population in 2010, by Country'
-wm.add('2010', cc_populations)
 
-wm.render_to_file('world_population.svg')
+# 根据人口数量将所有的国家分成三组
+cc_pops_1, cc_pops_2, cc_pops_3 = {}, {},{}
+for cc, pop in cc_populations.items():
+    if pop < 10000000:
+        cc_pops_1[cc] = pop
+    elif pop < 1000000000:
+        cc_pops_2[cc] = pop
+    else:
+        cc_pops_3[cc] = pop
+print(len(cc_pops_1), len(cc_pops_2), len(cc_pops_3))
+
+
+wm_style = RotateStyle('#336699',base_style=LightColorizedStyle)
+wm = pygal_maps_world.maps.World(style=wm_style)
+# wm = pygal_maps_world.maps.World()
+wm.title = 'World Population in 2010, by Country'
+# wm.add('2010', cc_populations)
+wm.add('0-10m', cc_pops_1)
+wm.add('10m-1bn', cc_pops_2)
+wm.add('>1bn', cc_pops_3)
+
+wm.render_to_file('world_population2.svg')
 
 
